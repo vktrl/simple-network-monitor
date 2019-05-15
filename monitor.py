0e4fec545781a6456ef5513579520baa3601db4e
Viktor Lillemäe <viktor@juhe.ee>
2019-05-15 11:11:05 +0300

    improvements

--- monitor.py
+++ monitor.py
@@ -1,44 +1,44 @@
 import os, re, time, sys, signal
 from datetime import datetime
 delay = 1
+failures = 0
+tolerance = 1
+host = "1.1.1.1"
 open('log.txt', 'a').close()
 
 if len(sys.argv) - 1 != 1:
-    print("Gib host")
-    exit()
+    print("No host given, defaulting to " + host)
 else:
     host = str(sys.argv[1])
 
 def current_time():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-def log(input):
+def write(message):
     with open("log.txt", "a") as log:
-        message = str(input + "\t" + current_time() + "\n")
-        log.write(message)
+        log.write(message + "\n")
         print(message)
-class connection:
-    def __init__(self, host, state):
-        self.state = state
-    def status(self, state):
-        self.state = state
-        log("link " + state)
-    def ping(self):
-        time.sleep(delay)
-        response = os.system("ping -c 1 " + host + " 1> /dev/null 2> /dev/null")
-        return response
+def ping():
+    response = os.system("ping -c 1 -W 1 " + host + " 1> /dev/null 2> /dev/null")
+    return response
 def signal_handler(signal, frame):
-  log("Stopped")
+  write("Stopped\t" + current_time())
   sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
 
-print("Monitoring " + host)
-log("---------------------------\nStarted")
-link = connection(host, "up")
+write("---------------------------\nHost: "+ host + "\nStarted\t" + current_time())
 
 while True:
-    if link.ping() != 0:
-        link.status("Dropped")
-        while link.ping() != 0:
-            time.sleep(1)
-        link.status("Restored")
+    if ping() != 0:
+        failures += 1
+        if failures == 1:
+            first_failure = current_time()
+    else:
+        failures = 0
+    time.sleep(delay)
+    while failures > tolerance:
+        write("Down\t" + first_failure)
+        while ping() != 0:
+            time.sleep(delay)
+        write("Up\t" + current_time())
+        failures = 0
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+tmux new-session -d -s simple-net-monitor 'python3 monitor.py'
