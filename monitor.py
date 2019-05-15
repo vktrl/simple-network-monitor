3c5132a39835d9cde6d94b70ad1e280dec7a0b20
Viktor Lillemäe <viktor@juhe.ee>
2019-05-15 15:37:29 +0300

    added time delta

--- monitor.py
+++ monitor.py
@@ -4,6 +4,7 @@ delay = 1
 failures = 0
 tolerance = 1
 host = "1.1.1.1"
+separator = "---------------------------"
 open('log.txt', 'a').close()
 
 if len(sys.argv) - 1 != 1:
@@ -21,12 +22,17 @@ def ping():
     response = os.system("ping -c 1 -W 1 " + host + " 1> /dev/null 2> /dev/null")
     return response
 def signal_handler(signal, frame):
-  write("Stopped\t" + current_time())
-  sys.exit(0)
+    write("Stopped\t" + current_time())
+    sys.exit(0)
+def time_difference(start, end):
+    d1 = datetime.strptime(start, '%Y-%m-%d %H:%M:%S')
+    d2 = datetime.strptime(end, '%Y-%m-%d %H:%M:%S')
+    return (d2 - d1).total_seconds()
 
 signal.signal(signal.SIGINT, signal_handler)
 
-write("---------------------------\nHost: "+ host + "\nStarted\t" + current_time())
+write("Host: "+ host + "\nStarted\t" + current_time())
+write(separator)
 
 while True:
     if ping() != 0:
@@ -41,4 +47,7 @@ while True:
         while ping() != 0:
             time.sleep(delay)
         write("Up\t" + current_time())
+        downtime = time_difference(first_failure, current_time())
+        write("Time\t" + str(round(downtime)) + " sec")
+        write(separator)
         failures = 0
